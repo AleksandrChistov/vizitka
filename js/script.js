2d44639bcaec308097a55e1e6eaac7f0ef6873c5
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2019-07-31 19:34:11 +0700

    Сделал header и первую сексию +адаптивность

---
+++ js/script.js
@@ -0,0 +1,40 @@
+$(function(){
+
+  var windowWidth = $(window).width();
+  if( windowWidth <= 800 ) {
+    $('.avatar__info-wrap').insertAfter($('.avatar'));
+    $('.avatar img').prependTo($('.avatar'));
+  }
+
+  window.addEventListener("resize", function() {
+		var windowWidth = $(window).width();
+		if( windowWidth <= 800 ) {
+      $('.avatar__info-wrap').insertAfter($('.avatar'));
+      $('.avatar img').prependTo($('.avatar'));
+		}else {
+      $('.avatar__info-wrap').prependTo($('.relative'));
+      $('.avatar img').prependTo($('.relative'));
+    }
+  }, false);
+
+  var windowWidth = $(window).width();
+  if( windowWidth <= 610 ) {
+    $('.сallback').text('');
+    $('.сallback').removeClass();
+    $('header form button').addClass('сallback-small');
+  }
+  
+  window.addEventListener("resize", function() {
+		var windowWidth = $(window).width();
+		if( windowWidth <= 610 ) {
+      $('.сallback').text('');
+      $('.сallback').removeClass();
+      $('header form button').addClass('сallback-small');
+		}else {
+      $('.сallback-small').text('Заказать звонок');
+      $('.сallback-small').removeClass();
+      $('header form button').addClass('сallback');
+    }
+  }, false);
+
+});
