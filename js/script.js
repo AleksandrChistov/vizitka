2ac7b98981c352a2b0efa270a5d139b7b7726959
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2019-08-15 17:20:25 +0700

    Тестирование touch

--- js/script.js
+++ js/script.js
@@ -72,5 +72,64 @@ $(function(){
         $('.work__nav-btn:first-child').removeClass('work__nav-btn_active');
     }
   });
+
+  if(slideCountWidth < 1064) {
+
+var initialPoint;
+var finalPoint;
+document.addEventListener('touchstart', function(event) {
+event.preventDefault();
+event.stopPropagation();
+initialPoint=event.changedTouches[0];
+}, false);
+document.addEventListener('touchend', function(event) {
+event.preventDefault();
+event.stopPropagation();
+finalPoint=event.changedTouches[0];
+var xAbs = Math.abs(initialPoint.pageX - finalPoint.pageX);
+var yAbs = Math.abs(initialPoint.pageY - finalPoint.pageY);
+if (xAbs > 20 || yAbs > 20) {
+if (xAbs > yAbs) {
+  var slideCount = $('.doing-and-work__item-wrap-2').children().length;
+if (finalPoint.pageX < initialPoint.pageX){
+  if (slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
+    $('.work__item').css('transform', 'translate(0, 0)');
+    slideNow = 1;
+    } else {
+      slideWidth = $('.work__item').width() + padding;
+      translateWidth = (-slideWidth) * (slideNow);
+            $('.work__item').css({
+                'transform': 'translate(' + translateWidth + 'px, 0)',
+                '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+                '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+            });
+            slideNow++;
+    }
+}
+else{
+  if (slideNow == 1 || slideNow <= 0 || slideNow > slideCount) {
+    translateWidth = (-$('.work__item').width() - padding) * (slideCount - 1);
+    $('.work__item').css({
+        'transform': 'translate(' + translateWidth + 'px, 0)',
+        '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+        '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+    });
+    slideNow = slideCount;
+} else {
+    translateWidth = (-$('.work__item').width() - padding) * (slideNow - 2);
+    $('.work__item').css({
+        'transform': 'translate(' + translateWidth + 'px, 0)',
+        '-webkit-transform': 'translate(' + translateWidth + 'px, 0)',
+        '-ms-transform': 'translate(' + translateWidth + 'px, 0)',
+    });
+    slideNow--;
+}
+}
+}
+}
+}, false);
+
   
+  }
+
 });
