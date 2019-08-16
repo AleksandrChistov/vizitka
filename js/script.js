a3f056ccec4658b1e39fe28065bfb356a4da7f7b
AleksandrChistov <aleksandrchistov.ru@yandex.ru>
2019-08-16 13:28:51 +0700

    Тест 4

--- js/script.js
+++ js/script.js
@@ -14,6 +14,8 @@ $(function(){
     prevSlide();
   });
 
+  
+
   function nextSlide() {
     if(slideNow == slideCount || slideNow <= 0 || slideNow > slideCount) {
       translateWidth = (slideWidth + 30) * 0;
@@ -47,11 +49,11 @@ $(function(){
         $('.work__item:eq(3), .work__item:eq(4)').remove();
         }, 700);
     } else {
-      var translateWidth = (-slideWidth - 30) * (slideNow);
+      var translateWidth2 = (-slideWidth - 30) * (slideNow);
       $('.work__item').css({
-        'transform': 'translate('+translateWidth+'px,0)',
-        '-o-transform': 'translate('+translateWidth+'px,0)',
-        '-webkit-transform': 'translate('+translateWidth+'px,0)'
+        'transform': 'translate('+translateWidth2+'px,0)',
+        '-o-transform': 'translate('+translateWidth2+'px,0)',
+        '-webkit-transform': 'translate('+translateWidth2+'px,0)'
       });
       $('.work__item').eq(slideFirst).clone().delay(700).queue(function() {
         $(this).appendTo('.doing-and-work__item-wrap-2');
